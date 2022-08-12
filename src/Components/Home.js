13126a6a51fb7702d1f88b39b94d1cbe0994ccfc
Zakaria <zakaria7262@gmail.com>
2022-08-12 23:27:53 +0600

    push 3td time

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>homepage running</h1>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam, officiis possimus? Alias, nam! Fuga magnam quidem culpa temporibus incidunt quam cupiditate non, quia impedit sit aliquid repellat qui id vel accusantium asperiores dignissimos! Quasi debitis veritatis nihil dolorem rem nobis rerum! Earum id harum alias distinctio perspiciatis ea maiores fugit.</p>
-        </div>
-    );
+  return (
+    <div>
+      <h1>homepage running</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam,
+        officiis possimus? Alias, nam! Fuga magnam quidem culpa temporibus
+        incidunt quam cupiditate non, quia impedit sit aliquid repellat qui id
+        vel accusantium asperiores dignissimos! Quasi debitis veritatis nihil
+        dolorem rem nobis rerum! Earum id harum alias distinctio perspiciatis ea
+        maiores fugit.
+      </p>
+    </div>
+  );
 };
 
 export default Home;
--- src/Components/Rahat.js
+++ src/Components/Rahat.js
@@ -5,6 +5,7 @@ const Rahat = () => {
         <div>
             <h2>This is rahat</h2>
             <p>some time add something</p>
+            <h1>This is zakaria</h1>
         </div>
     );
 };
